8aa1dea1b6e0c4ff4bf1b112cc45101b6b833103
Kristina <khalitovaki@yandex.ru>
2023-08-03 11:27:42 +0500

    Удалены импорты

--- src/main/java/ru/practicum/shareit/item/dto/ResponseItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ResponseItemDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.*;
 import ru.practicum.shareit.booking.dto.BookingDtoWithBookers;
-import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
